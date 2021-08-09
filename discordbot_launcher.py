021dc8cfa3161ac6b6d78b34b48d5ae5ab7e354b
Евгений <user-vo2@yandex.ru>
2021-08-09 14:53:48 +0300

    master to main

--- discordbot_launcher.py
+++ discordbot_launcher.py
@@ -2,8 +2,7 @@ import os
 
 os.chdir('c:\\musicbot')
 os.system('ssh -T git@github.com')
-os.system('git remote add origin git@github.com:user-vo2/discord-bot.git')
-os.system('git pull origin master')
+os.system('git pull origin main')
 print('Update check finished. Launching bot...')
 os.system('c:\\python38\\python -O c:\\musicbot\\musicbot.py')
 exit(0)
